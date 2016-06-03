8cb6f0314ad1fccae048ce24d91a7d116ff090f8
Andrew Aikman <a.aikman@ic.ac.uk>
2016-06-03 11:59:18 +0100

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'aik_deployment_tool',
+    version = '0.1',
+    packages = [''],
+    url = '',
+    license = '',
+    author = 'aiky30',
+    author_email = 'a.aikman@aikman-ltd.uk',
+    description = 'A Python deployment tool'
+)
